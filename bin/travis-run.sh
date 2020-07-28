249a3d077141760581e10b73e148245eebdde38c
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-07-28 13:50:53 +0100

    ADX-348 Fix travis run bug.

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,8 +3,7 @@ set -ex
 
 flake8 --version
 # stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,{{ project }}
-
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan
 nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
@@ -16,4 +15,4 @@ nosetests --ckan \
           ckanext/ytp/request
 
 # strict linting
-flake8 . --count --max-complexity=12 --max-line-length=127 --statistics --exclude ckan,{{ project }}
+flake8 . --count --max-complexity=12 --max-line-length=127 --statistics --exclude ckan
