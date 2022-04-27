fa8b29d0a8f4b78c3d7383fae7dc165f287e2c75
Tomasz Sabała <tomek@fjelltopp.org>
2022-04-27 10:13:36 +0200

    Fix sqlalchemy model configuration and db test setup.

--- ckanext/ytp_request/model.py
+++ ckanext/ytp_request/model.py
@@ -1,15 +1,17 @@
+import logging
 import uuid
 import datetime
+import six
 
 from sqlalchemy import Column, MetaData, ForeignKey
 from sqlalchemy import types
 from sqlalchemy.ext.declarative import declarative_base
 
 from ckan.lib.base import model
+from ckan.model.meta import metadata
 
-log = __import__('logging').getLogger(__name__)
-Base = declarative_base()
-metadata = MetaData()
+log = logging.getLogger(__name__)
+Base = declarative_base(metadata=metadata)
 
 """CANCEL state is equivalent to DELETE state in member table.
 member - member_request is one to many relationship since we need to log all member_requests to facilitate admins and users
@@ -21,7 +23,7 @@ REQUEST_CANCEL = "cancel"
 
 
 def make_uuid():
-    return str(uuid.uuid4())
+    return six.text_type(uuid.uuid4())
 
 
 class MemberRequest(Base):
@@ -52,4 +54,8 @@ class MemberRequest(Base):
 
 
 def init_tables():
-    Base.metadata.create_all(model.meta.engine)
+    MemberRequest.__table__.create()
+
+
+def tables_exist():
+    return MemberRequest.__table__.exists()
--- ckanext/ytp_request/tests/test_plugin.py
+++ ckanext/ytp_request/tests/test_plugin.py
@@ -4,22 +4,10 @@ import pytest
 import mock
 from ckan.lib.helpers import url_for
 
-import ckan.model as model
-import ckanext.ytp_request.model as rmodel
-
 import ckan.tests.helpers as helpers
 import ckan.tests.factories as factories
 
 
-@pytest.fixture
-def initdb():
-    model.Session.remove()
-    model.Session.configure(bind=model.meta.engine)
-    rmodel.init_tables()
-
-
-@pytest.mark.usefixtures(u'initdb')
-@pytest.mark.usefixtures(u'clean_db')
 @pytest.mark.ckan_config(u'ckan.plugins', u'ytp_request')
 @pytest.mark.usefixtures(u'with_plugins')
 @pytest.mark.usefixtures(u'with_request_context')
