cc2c9df0332611bc85c02d5ba15b6c7630990c51
Tomasz Sabała <tomek@fjelltopp.org>
2022-05-05 09:53:26 +0200

    Fixing translations.

--- ckanext/ytp_request/mail.py
+++ ckanext/ytp_request/mail.py
@@ -8,68 +8,71 @@ log = logging.getLogger(__name__)
 
 def _SUBJECT_MEMBERSHIP_REQUEST():
     return _(
-        "New membership request (%(organization)s)")
+            "New membership request (%(organization)s)")
 
 
 def _MESSAGE_MEMBERSHIP_REQUEST():
-    return _("""\
-User %(user)s (%(email)s) has requested membership to organization %(organization)s.
+    return _(
+        """\
+        User %(user)s (%(email)s) has requested membership to organization %(organization)s.
 
-%(link)s
+        %(link)s
 
-Best regards
-
-Avoindata.fi support
-avoindata@dvv.fi
-""")
+        Best wishes,
+        The AIDS Data Repository
+        """
+    )
 
 
 def _SUBJECT_MEMBERSHIP_APPROVED():
     return _(
-        "Organization membership approved (%(organization)s)")
+        "Organization membership approved (%(organization)s)"
+    )
 
 
 def _MESSAGE_MEMBERSHIP_APPROVED():
-    return _("""\
-Your membership request to organization %(organization)s with %(role)s access has been approved.
+    return _(
+        """\
+        Your membership request to organization %(organization)s with %(role)s
+        access has been approved.
 
-Best regards
-
-Avoindata.fi support
-avoindata@dvv.fi
-""")
+        Best wishes,
+        The AIDS Data Repository
+        """
+    )
 
 
 def _SUBJECT_MEMBERSHIP_REJECTED():
     return _(
-        "Organization membership rejected (%(organization)s)")
+        "Organization membership rejected (%(organization)s)"
+    )
 
 
 def _MESSAGE_MEMBERSHIP_REJECTED():
-    return _("""\
-Your membership request to organization %(organization)s with %(role)s access has been rejected.
+    return _(
+        """\
+        Unfortunately your membership request to organization %(organization)s
+        with %(role)s access has been rejected.  If you think this was a
+        mistake, please contact the organisation's administrator directly.
 
-Best regards
-
-Avoindata.fi support
-avoindata@dvv.fi
-""")
+        Best wishes,
+        The AIDS Data Repository
+        """
+    )
 
 
 def mail_new_membership_request(locale, admin, group_name, url, user_name, user_email):
-    # TODO: Set admin locale. Admin/user locale is stored at drupal database so may be a bit challenging to fetch it.
-    # We default to finnish for the time being
 
-    with force_locale('fi'):
-        subject = _SUBJECT_MEMBERSHIP_REQUEST() % {
-            'organization': group_name
-        }
-        message = _MESSAGE_MEMBERSHIP_REQUEST() % {
-            'user': user_name,
-            'email': user_email,
-            'organization': group_name,
-            'link': url
-        }
+    subject = _SUBJECT_MEMBERSHIP_REQUEST() % {
+        'organization': group_name
+    }
+    message = _MESSAGE_MEMBERSHIP_REQUEST() % {
+        'user': user_name,
+        'email': user_email,
+        'organization': group_name,
+        'link': url
+    }
+
     try:
         mail_user(admin, subject, message)
     except Exception:
