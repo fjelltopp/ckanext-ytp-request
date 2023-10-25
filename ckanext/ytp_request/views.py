61a644c32e37730994f5e0daad6557d4ad4151a0
Jonathan S Berry <jonathan.s.berry@gmail.com>
2023-10-25 09:29:51 +0100

    ADX-1062 Default to member role when requesting to join org

--- ckanext/ytp_request/views.py
+++ ckanext/ytp_request/views.py
@@ -38,7 +38,7 @@ def new(errors=None, error_summary=None):
     selected_organization = toolkit.request.args.get(
         'selected_organization', None)
     roles = _get_available_roles(context, selected_organization)
-    user_role = 'admin'
+    user_role = 'member'
 
     extra_vars = {'selected_organization': selected_organization, 'organizations': organizations,
                   'errors': errors or {}, 'error_summary': error_summary or {},
